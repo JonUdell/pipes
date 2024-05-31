48d797c8e93cb34ed5659a6892b5a926dea6fe4f
Jon Udell <jon@jonudell.info>
2024-05-31 12:03:09 -0700

    wip

--- connections.pp
+++ connections.pp
@@ -18,20 +18,67 @@ dashboard "Connections" {
     }
   }  
 
-
   table {
     title = "Connections"
+    width = 4
     sql = <<EOQ
+      with cte as (
+          select
+              plugin,
+              handle || '/' || identity_handle as org_workspace,
+              row_number() over (partition by plugin order by handle, identity_handle) as row_num,
+              count(*) over (partition by plugin) as plugin_count,
+              dense_rank() over (order by plugin) as dense_rank
+          from
+              pipes_connection
+      )
       select
-        plugin,
-        handle,
-        identity_handle
-      from
-        pipes.pipes_connection
-      order by
-        plugin, identity_handle
+          case
+              when row_num = 1 then plugin || ' (' || plugin_count || ')'
+              else ''
+          end as plugin,
+          org_workspace
+      FROM
+          cte
+      ORDER BY
+          dense_rank,
+          row_num;
     EOQ
   }
+
+  table {
+    title = "By plugin"
+    width = 2
+    sql = <<EOQ
+      select
+          plugin,
+          count(*)
+      from
+          pipes_connection
+      group by
+          plugin
+      order by
+          plugin
+    EOQ
+  }
+
+table {
+    title = "By count"
+    width = 2
+    sql = <<EOQ
+      select
+          plugin,
+          count(*)
+      from
+          pipes_connection
+      group by
+          plugin
+      order by
+          count desc
+    EOQ
+}
+    
+
 
 }
 
--- workspaces.pp
+++ workspaces.pp
@@ -27,22 +27,22 @@ dashboard "Workspaces" {
           from pipes.pipes_organization
       )
       select 
-        org_handle,
-        w.handle as w_handle,
-        w.id as w_id,
-        display_name,
+        org_handle || '/' || w.handle as org_workspace,
+        display_name as workspace_owner,
         status,
         database_name,
         state,
         created_by->>'display_name' as w_created_by,
         cli_version,
-        api_version
+        api_version,
+        w.id as w_id,
+        *
       from org_data o 
       left join pipes.pipes_user p on o.created_by_id = p.id
       join pipes.pipes_workspace w on o.org_id = w.identity_id
       order by org_handle, w.handle
       EOQ
-    column "w_id" {
+    column "org_workspace" {
       href = "${local.host}/pipes.dashboard.Workspace_Detail?input.workspace_ids={{.'w_id'}}"      
     }  
 

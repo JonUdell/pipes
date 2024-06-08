a30b97e1191cadef37f90296f2a597c1b5eab13b
judell <jon@jonudell.info>
2024-06-08 05:01:09 -0700

    tweak

--- mods.pp
+++ mods.pp
@@ -31,17 +31,17 @@ dashboard "Mods" {
             'pipes_workspace_mod',
             'identity_handle',
             'identity_handle, workspace_handle',
-            array['workspace_handle', 'alias', 'installed_version', 'state', 'updated_at', 'created_by']
+            array['identity_handle', 'workspace_handle', 'alias', 'installed_version', 'state', 'updated_at', 'created_by']
           ) as json_data
       )
       select
         json_data->>'display_partition_column' as org,
-        json_data->'additional_columns'->>0 as workspace,
-        json_data->'additional_columns'->>1 as alias,
-        json_data->'additional_columns'->>2 as installed_version,
-        json_data->'additional_columns'->>3 as state,
-        json_data->'additional_columns'->>4 as updated_at,
-        json_data->'additional_columns'->5->>'display_name' as created_by
+        concat(json_data->'additional_columns'->>0, '/',json_data->'additional_columns'->>1) as org_workspace,
+        json_data->'additional_columns'->>2 as alias,
+        json_data->'additional_columns'->>3 as installed_version,
+        json_data->'additional_columns'->>4 as state,
+        json_data->'additional_columns'->>5 as updated_at,
+        json_data->'additional_columns'->6->>'display_name' as created_by
       from
         data
       EOQ
